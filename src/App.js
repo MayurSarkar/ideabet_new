5cc3cbf95b4a462b2b57dd07aea35b1b968c5237
Unknown Coder <ankitkumarj143@gmail.com>
2023-07-13 00:11:26 +0530

    Example of tailwind

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './css/App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="flex justify-center align-middle w-screen h-10">
       hello world
       its a new age for you
     </div>
